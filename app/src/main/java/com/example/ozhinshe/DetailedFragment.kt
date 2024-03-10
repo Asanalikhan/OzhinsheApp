3e407d56aa1421fe95003316ea01533d47c1c6a6
Assanalikhan <210103042@stu.sdu.edu.kz>
2024-03-10 21:36:09 +0500

    initRetrofit

--- app/src/main/java/com/example/ozhinshe/DetailedFragment.kt
+++ app/src/main/java/com/example/ozhinshe/DetailedFragment.kt
@@ -1,16 +1,22 @@
 package com.example.ozhinshe
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.ozhinshe.data.MainApi
 import com.example.ozhinshe.databinding.FragmentDetailedBinding
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class DetailedFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailedBinding
-    private var isTextVisible = true
+    private lateinit var mainApi: MainApi
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +29,19 @@ class DetailedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initRetrofit()
+
+        val sharedPreferences = requireContext().getSharedPreferences("Authotification", Context.MODE_PRIVATE)
+        val token = sharedPreferences.getString("token_key", null)
+
+    }
+    private fun initRetrofit(){
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+        val BASE_URL = "http://api.ozinshe.com"
+        val retrofit: Retrofit = Retrofit.Builder().baseUrl(BASE_URL).
+        client(client).addConverterFactory(GsonConverterFactory.create()).build()
+        mainApi = retrofit.create(MainApi::class.java)
     }
 }

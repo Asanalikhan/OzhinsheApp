5394306d43e91072dc4765a315e4f24bb50f1b62
Assanalikhan <210103042@stu.sdu.edu.kz>
2024-06-04 15:11:39 +0500

    DB getting from server

--- app/src/main/java/com/example/ozhinshe/BookmarkFragment.kt
+++ app/src/main/java/com/example/ozhinshe/BookmarkFragment.kt
@@ -8,10 +8,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.ozhinshe.data.Item
 import com.example.ozhinshe.data.ItemViewModel
 import com.example.ozhinshe.data.MainApi
 import com.example.ozhinshe.databinding.FragmentBookmarkBinding
+import com.example.ozhinshe.modiedata.Movy
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -22,7 +28,6 @@ class BookmarkFragment : Fragment() {
     private lateinit var binding: FragmentBookmarkBinding
     private lateinit var mainApi: MainApi
     private lateinit var itemViewModel: ItemViewModel
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,27 +40,44 @@ class BookmarkFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initRetrofit()
+
         itemViewModel = ViewModelProvider(this).get(ItemViewModel::class.java)
         itemViewModel.getAllData.observe(viewLifecycleOwner, Observer { items ->
             getAllData(items)
+
         })
 
 
-//        lifecycleScope.launch(Dispatchers.IO) {
-//            var response = mainApi.getMovie(id, )
-//        }
     }
 
-    fun getAllData(listItems: List<Item>){
-        var listItem = listItems
-        initRetrofit()
-        val token = getToken()
-//        lifecycleScope.launch(Dispatchers.IO) {
-//            var response = mainApi.getMovie(listItem.)
-//        }
-
+    private fun getAllData(listItems: List<Item>) {
+        var listMovy = mutableListOf<Movy>()
+        var token = getToken()
+        val deferredMovies = mutableListOf<Deferred<Movy>>()
+        for (item in listItems) {
+            deferredMovies.add(
+                lifecycleScope.async(Dispatchers.IO) {
+                    val response = mainApi.getMovie(item.id?.toInt(), "Bearer $token")
+                    response
+                }
+            )
+        }
+        val allMovies = lifecycleScope.async(Dispatchers.IO) {
+            val movies = mutableListOf<Movy>()
+            deferredMovies.forEach { deferredMovie ->
+                movies.add(deferredMovie.await())
+            }
+            movies
+        }
+        lifecycleScope.launch(Dispatchers.Main) {
+            val finalMovies = allMovies.await()
+            listMovy.addAll(finalMovies)
+            binding.eddd.text = listMovy[12].name
+        }
     }
-    fun initRetrofit(){
+
+    private fun initRetrofit(){
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
         val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
@@ -64,7 +86,7 @@ class BookmarkFragment : Fragment() {
             GsonConverterFactory.create()).build()
         mainApi = retrofit.create(MainApi::class.java)
     }
-    fun getToken(): String{
+    private fun getToken(): String{
         val sharedPreferences = requireContext().getSharedPreferences("Authotification", Context.MODE_PRIVATE)
         val token = sharedPreferences.getString("token_key", null)
         return token.toString()

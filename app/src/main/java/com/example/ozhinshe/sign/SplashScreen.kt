de3373539b6376cbf7a03a3996bc09316c312fdc
Assanalikhan <210103042@mail.com>
2024-06-20 13:45:11 +0500

    Skip onboard

--- app/src/main/java/com/example/ozhinshe/sign/SplashScreen.kt
+++ app/src/main/java/com/example/ozhinshe/sign/SplashScreen.kt
@@ -1,15 +1,36 @@
 package com.example.ozhinshe.sign
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.ozhinshe.HomeActivity
 import com.example.ozhinshe.R
+import com.example.ozhinshe.SearchFragment
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val sharedPreferences = getSharedPreferences("Authotification", Context.MODE_PRIVATE)
+        val token = sharedPreferences.getString("token_key", null)
+
+        if(token == null){
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        }
+        else{
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            val navController = navHostFragment.navController
+            navController.navigate(R.id.action_onBoardFragment_to_authorizationFragment)
+//            val intent = Intent(this@SplashScreen, HomeActivity::class.java)
+//            startActivity(intent)
+//            finish()
+        }
+
     }
 }

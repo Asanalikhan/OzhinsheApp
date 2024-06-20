d1a4de9e33a0e5b33ffa7f6b19cb937111e1c8e9
Assanalikhan <210103042@mail.com>
2024-06-20 12:54:18 +0500

    QupiyaFragment finished

--- app/src/main/java/com/example/ozhinshe/profile/QupiyaFragment.kt
+++ app/src/main/java/com/example/ozhinshe/profile/QupiyaFragment.kt
@@ -1,10 +1,12 @@
 package com.example.ozhinshe.profile
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.ozhinshe.HomeActivity
 import com.example.ozhinshe.R
 import com.example.ozhinshe.databinding.FragmentQupiyaBinding
@@ -23,8 +25,30 @@ class QupiyaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.errorMsg.visibility = View.GONE
         binding.imageButton.setOnClickListener {
             (activity as? HomeActivity)?.replaceFragment(ProfileFragment())
         }
+        binding.savePassword.setOnClickListener {
+            if(isPasswordValid()){
+                val sharedPreferences = requireContext().getSharedPreferences("Profile", Context.MODE_PRIVATE)
+                val editor = sharedPreferences.edit()
+                editor.putString("password", binding.textInputLayout1.editText?.text.toString().trim())
+                editor.apply()
+                Toast.makeText(requireContext(), "Құпия сөз сәтті сақталды", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun isPasswordValid():Boolean {
+        val password1 = binding.textInputLayout1.editText?.text.toString().trim()
+        val password2 = binding.textInputLayout2.editText?.text.toString().trim()
+        if(password1.length >= 6 && password2 == password1){
+            return true
+        }else {
+            binding.errorMsg.visibility = View.VISIBLE
+        }
+        return false
     }
 }

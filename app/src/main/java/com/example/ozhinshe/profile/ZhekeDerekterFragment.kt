d3fb74a84cf163d9001a349dff79ba589701d4c9
Assanalikhan <210103042@mail.com>
2024-06-20 12:28:47 +0500

    Zheke Derekter

--- app/src/main/java/com/example/ozhinshe/profile/ZhekeDerekterFragment.kt
+++ app/src/main/java/com/example/ozhinshe/profile/ZhekeDerekterFragment.kt
@@ -1,6 +1,9 @@
 package com.example.ozhinshe.profile
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -27,6 +30,38 @@ class ZhekeDerekterFragment : Fragment() {
         binding.imageButton.setOnClickListener {
             (activity as? HomeActivity)?.replaceFragment(ProfileFragment())
         }
+
+        loadData()
+
+        binding.saveData.setOnClickListener {
+            saveData()
+        }
+
+    }
+    private fun saveData(){
+        val profileSharedPreferences = requireContext().getSharedPreferences("Profile", Context.MODE_PRIVATE)
+        val editor = profileSharedPreferences.edit()
+        editor.apply{
+            putString("name", binding.fullName.text.toString())
+            putString("email", binding.tvEmailText.text.toString())
+            putString("phonenumber", binding.phoneNumber.text.toString())
+            putString("date", binding.birthDate.text.toString())
+            apply()
+        }
+    }
+
+    private fun loadData(){
+        val profileSharedPreferences = requireContext().getSharedPreferences("Profile", Context.MODE_PRIVATE)
+
+        val name = profileSharedPreferences.getString("name", "")
+        val email = profileSharedPreferences.getString("email", "")
+        val phone = profileSharedPreferences.getString("phonenumber", "")
+        val birth = profileSharedPreferences.getString("date", "")
+
+        binding.fullName.text = Editable.Factory.getInstance().newEditable(name)
+        binding.tvEmailText.text = Editable.Factory.getInstance().newEditable(email)
+        binding.phoneNumber.text = Editable.Factory.getInstance().newEditable(phone)
+        binding.birthDate.text = Editable.Factory.getInstance().newEditable(birth)
 
     }
 }

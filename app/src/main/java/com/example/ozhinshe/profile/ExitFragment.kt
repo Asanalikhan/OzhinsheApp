89602a3dc498d851bb6e6141f1cd47a4f59c2e51
Assanalikhan <210103042@mail.com>
2024-06-20 14:44:04 +0500

    Profile

--- app/src/main/java/com/example/ozhinshe/profile/ExitFragment.kt
+++ app/src/main/java/com/example/ozhinshe/profile/ExitFragment.kt
@@ -1,23 +1,45 @@
 package com.example.ozhinshe.profile
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
 import com.example.ozhinshe.R
+import com.example.ozhinshe.databinding.ExitFragmentBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import androidx.navigation.fragment.findNavController
 
 class ExitFragment: BottomSheetDialogFragment() {
+
+    private lateinit var binding: ExitFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.exit_fragment, container, false)
+        binding = ExitFragmentBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        binding.yes.setOnClickListener {
+            exit()
+            requireActivity().finishAffinity()
+        }
+        binding.no.setOnClickListener {
+            dismiss()
+        }
+    }
+    private fun exit(){
+        val sharedPreferences = requireContext().getSharedPreferences("Authotification", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        editor.putString("token_key", "loged out")
+        editor.apply()
     }
 }
--- app/src/main/java/com/example/ozhinshe/sign/SplashScreen.kt
+++ app/src/main/java/com/example/ozhinshe/sign/SplashScreen.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -11,11 +13,24 @@ import androidx.navigation.fragment.findNavController
 import com.example.ozhinshe.HomeActivity
 import com.example.ozhinshe.R
 import com.example.ozhinshe.SearchFragment
+import com.example.ozhinshe.data.MainApi
+import com.example.ozhinshe.modiedata.AuthRequest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SplashScreen : AppCompatActivity() {
+
+    private lateinit var mainApi: MainApi
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        initRetrofit()
 
         val sharedPreferences = getSharedPreferences("Authotification", Context.MODE_PRIVATE)
         val token = sharedPreferences.getString("token_key", null)
@@ -23,14 +38,55 @@ class SplashScreen : AppCompatActivity() {
         if(token == null){
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         }
-        else{
+        else if(token == "loged out"){
             val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
             val navController = navHostFragment.navController
             navController.navigate(R.id.action_onBoardFragment_to_authorizationFragment)
-//            val intent = Intent(this@SplashScreen, HomeActivity::class.java)
-//            startActivity(intent)
-//            finish()
         }
-
+        else{
+            login()
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            val navController = navHostFragment.navController
+            navController.navigate(R.id.action_onBoardFragment_to_authorizationFragment)
+        }
+    }
+    private fun login(){
+        val sharedPreferences = getSharedPreferences("Profile", Context.MODE_PRIVATE)
+        val email = sharedPreferences.getString("email", null)
+        val password = sharedPreferences.getString("password", null)
+        auth(AuthRequest(email.toString(), password.toString())) { success ->
+            if (success) {
+                val intent = Intent(this@SplashScreen, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+    private fun auth(authRequest: AuthRequest, onComplete: (Boolean) -> Unit){
+        lifecycleScope.launch(Dispatchers.IO){
+            val responce = mainApi.auth(authRequest)
+            lifecycleScope.launch(Dispatchers.Main) {
+                if(responce.errorBody() != null){
+                    onComplete(false)
+                }
+                else{
+                    onComplete(true)
+                }
+                val user = responce.body()
+                val sharedPreferences = getSharedPreferences("Authotification", Context.MODE_PRIVATE)
+                val editor = sharedPreferences.edit()
+                editor.putString("token_key", user?.accessToken)
+                editor.apply()
+            }
+        }
+    }
+    private fun initRetrofit(){
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+        val BASE_URL = "http://api.ozinshe.com"
+        val retrofit: Retrofit = Retrofit.Builder().baseUrl(BASE_URL).
+        client(client).addConverterFactory(GsonConverterFactory.create()).build()
+        mainApi = retrofit.create(MainApi::class.java)
     }
 }
